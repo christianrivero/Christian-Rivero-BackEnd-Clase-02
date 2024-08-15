6b969a08409e29e0fb7fa7442aa802d9d342dae7
Christian Rivero <ch.riverovalencia@gmail.com>
2024-08-14 21:45:30 -0500

    Solución a 3 ejercicios

---
+++ script.js
@@ -0,0 +1,23 @@
+// Ejercicio 1
+console.log("EJERCICIO #1");
+let valor = 300, porcentaje = 25;
+console.log("El "+porcentaje+" de "+valor+" es "+(porcentaje*valor/100));
+
+// Ejercicio 2
+console.log("\nEJERCICIO #2");
+let numero = 17;
+
+if(numero%2 == 0)
+    console.log(numero+ " es par.")
+else
+    console.log(numero+ " es impar.")
+
+// Ejercicio 3
+console.log("\nEJERCICIO #3");
+let contador = 0, incrementado, decrementado;
+
+incrementado = contador++;
+decrementado = contador--;
+
+console.log("Valor incrementado: "+ incrementado);
+console.log("Valor decrementado: "+ decrementado);
